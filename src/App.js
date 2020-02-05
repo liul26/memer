0e9d529316ef21f2fe59f29ee0ad7a12b768053c
unknown <liul26@uw.edu>
2020-02-05 12:59:22 -0800

    assignment 4 added new features

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React, {useState} from 'react';
 import './App.css';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import { Search } from '@material-ui/icons';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 
 function App() {
   const [text, setText] = useState('')
   const [memes, setMemes] = useState([])
+  const [loading, setLoading] = useState(false)
   
   async function getMemes(){
+    setLoading(true)
     const key='AitgzNZBJNonm2fIFf7fc6EsGIuRfS0a'
     let url = 'https://api.giphy.com/v1/gifs/search?'
     url += 'api_key=' + key
@@ -17,42 +21,55 @@ function App() {
     const body = await r.json()
     setMemes(body.data)
     setText('')
+    setLoading(false)
+
   }
 
   console.log(memes)
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="input-wrap">
-          <TextField fullWidth variant="outlined" 
-            label="Search for memes!"
-            value={text} /*IMPORTANT*/
-            onChange={e=> setText(e.target.value)} /*IMPORTANT*/
-            onKeyPress={e=> {if(e.key==='Enter') getMemes()}}
-          />
-          <Button variant="contained" color="primary" onClick={getMemes}>
-            Search
-          </Button>
-          {/* <div></div> */}
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="input-wrap">
+            <TextField fullWidth variant="outlined" 
+              label="Search for memes!"
+              value={text} /*IMPORTANT*/
+              onChange={e=> setText(e.target.value)} /*IMPORTANT*/
+              onKeyPress={e=> {if(e.key==='Enter') getMemes()}}
+            />
+            <Button variant="contained" color="primary" onClick={getMemes}>
+              Search
+              <Search style={{height:30, width:30}}/>
+            </Button>
+            {/* <div></div> */}
+          </div>
+        </header>
+        <div className="memes">
+          {memes.map((meme, i)=> <Meme key={i} {...meme} />)}
+          <div className="loading-wrap">
+            {loading && <CircularProgress color='secondary' className='loading-circle'/>}</div>
         </div>
-
-      </header>
-
-      <div className="memes">
-        {memes.map((meme, i)=> <Meme key={i} {...meme} />)}
       </div>
-    </div>
-  );
+    );
 }
 
 function Meme({title, images}){
+  const url = images.fixed_height.url
   return (
-  <div className = "meme">
+    <div className="meme" onClick={()=>window.open(url, '_blank')}>
+     {/* images.fixed_height. <-- also ok to take out below */}
     <img src={images.fixed_height.url} alt="meme" />
-    <div className="meme-title">{title}</div> {/*SEE GITHUB*/}
+    <div className="meme-title">{title}</div>
   </div>)
 }
 
+// function Meme({images, title}){
+//   const {meme} = props
+//   const url = meme.images.fixed_height.url
+//   return (<div className="meme" onClick={()=>window.open(url, '_blank')}>
+//     <div className="meme-title">{title}</div>
+//     <img height="200" alt="meme" src={url} />
+//   </div>)
+// }
 
 export default App;
